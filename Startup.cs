09b9e38b7168bdc697beadde3e533f0567606f6b
Phil Krause <philipkrause7@gmail.com>
2022-09-23 01:06:31 -0400

    changes

--- Startup.cs
+++ Startup.cs
@@ -99,10 +99,10 @@ namespace mtrack
       {
         spa.Options.SourcePath = "ClientApp";
 
-        if (env.IsDevelopment())
-        {
-          spa.UseReactDevelopmentServer(npmScript: "start");
-        }
+        //if (env.IsDevelopment())
+       // {
+        //  spa.UseReactDevelopmentServer(npmScript: "start");
+       // }
       });
     }
   }
--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,11 @@
 dotnet publish -c Release 
 
-cp dockerfile ./bin/release/netcoreapp2.2/publish
+cp dockerfile ./bin/release/netcoreapp3.1/publish
 
-docker build -t mtrack-image  ./bin/release/netcoreapp2.2/publish
+docker build -t mtrack-image  ./bin/release/netcoreapp3.1/publish
 
 docker tag mtrack-image registry.heroku.com/mflighttrack/web
 
 heroku container:push web --app mflighttrack
 
 heroku container:release web -a mflighttrack
-
-# sudo chmod 755 deploy.sh
-# ./deploy.sh
